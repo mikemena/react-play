b2efdcafd0bb9716e6f070b869a46340125a6b00
tokyo <tokyolabs@protonmail.com>
2024-02-09 18:27:50 -0500

    wip

--- react-playground-app/src/components/counter.jsx
+++ react-playground-app/src/components/counter.jsx
@@ -3,10 +3,35 @@ import { useState } from "react";
 export default function Counter() {
   const [count, setCount] = useState(0);
 
-  const [rangeValue, setRangeValue] = useState(5);
+  const [rangeValue, setRangeValue] = useState(1);
 
-  const date = new Date("July 20 2030");
+  const today = new Date();
+  const hoy = formatDate(today);
+  const date = new Date(hoy);
   date.setDate(date.getDate() + count);
+
+  function formatDate(date) {
+    const monthNames = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+
+    const day = date.getDate();
+    const monthIndex = date.getMonth();
+    const year = date.getFullYear();
+
+    return `${monthNames[monthIndex]} ${day} ${year}`;
+  }
 
   function handleMinusCount() {
     setCount((c) => c - rangeValue);
